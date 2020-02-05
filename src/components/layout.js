ce76ddfbffd110c950b71b17fa46080479f4beda
Michael Wanyoike <micwanyoike@gmail.com>
2020-02-05 14:17:19 +0300

    Bulma partial layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import Nav from "./nav"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,15 +25,10 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+    <div className="app">
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <Nav />
+      <div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
@@ -40,7 +36,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,11 @@
 import React from "react"
-import "../styles.scss"
+
+import Layout from "../components/layout";
 
 const IndexPage = () => (
-  <div className="container">
-    <div className="columns">
-      <div className="column">
-        <h2 className="title is-2">Level 2 heading</h2>
-        <p className="content">Cool content. Using Bulma!</p>
-      </div>
-      <div className="column is-four-fifths">
-        <h2 className="title is-2">Level 2 heading</h2>
-        <p className="content">This column is cool too!</p>
-      </div>
-    </div>
-  </div>
+  <Layout>
+    <p>Hello World</p>
+  </Layout>
 )
 
 export default IndexPage
