7754cc62d25af31180b6315b921f245839c1c01f
Michael Wanyoike <micwanyoike@gmail.com>
2020-02-05 21:31:48 +0300

    Implemented basic query with react gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,46 @@
-import React from "react"
+import React from "react";
+import { graphql } from 'gatsby';
+import Gallery from "react-photo-gallery";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Overview" />
-    <p>Overview Photos</p>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { edges } = data.allStrapiImage;
+  const photos = edges.map(document => (
+    {
+      src: document.node.url.childImageSharp.fluid.src,
+      width: 4,
+      height: 3
+    }
+  ))
+  return (
+    <Layout>
+      <SEO title="Overview" />
+      <p>Overview Photos</p>
+      <Gallery photos={photos} />
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query AllImagesQuery {
+    allStrapiImage {
+      edges {
+        node {
+          id
+          title
+          url {
+            childImageSharp {
+              fluid(maxWidth: 400) {
+              ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
