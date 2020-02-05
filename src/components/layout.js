e463615eb927fdcb613f2552e1d228bfc6c9710b
Michael Wanyoike <micwanyoike@gmail.com>
2020-02-05 15:19:42 +0300

    Implemented basic navigation layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Container, Menu, } from 'semantic-ui-react'
+import { Menu, Grid } from 'semantic-ui-react'
 import "./layout.scss"
 
 const Layout = ({ children }) => {
 
   return (
     <div className="app">
+      {/* Top Menu */}
       <Menu fixed='top' inverted>
-        <Menu.Item as='a' header>
+        <Menu.Item as='a' href="/" header>
           Business Name
         </Menu.Item>
       </Menu>
-      <Container style={{ marginTop: '4em' }}>
-        <main>{children}</main>
-      </Container>
+
+      <Grid style={{ marginTop: '1em' }}>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Menu pointing secondary vertical>
+              <Menu.Item name='home' active />
+              <Menu.Item name='Animals' />
+              <Menu.Item name='Landscapes' />
+            </Menu>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <main>{children}</main>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   )
 }
