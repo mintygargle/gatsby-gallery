59e2d553c0d0c018138390d441127a25ee56013c
Michael Wanyoike <micwanyoike@gmail.com>
2020-02-06 13:53:23 +0300

    Refactored lightbox gallery

---
+++ src/components/lightbox-gallery.js
@@ -0,0 +1,45 @@
+import React, { useState, useCallback } from "react";
+import PropTypes from "prop-types"
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+const LightboxGallery = ({ photos }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  )
+}
+
+LightboxGallery.propTypes = {
+  photos: PropTypes.array.isRequired,
+}
+
+export default LightboxGallery;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import { Header } from 'semantic-ui-react'
 import { graphql } from 'gatsby';
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import LightboxGallery from "../components/lightbox-gallery";
 
 const IndexPage = ({ data }) => {
   const { edges } = data.allStrapiImage;
@@ -17,38 +16,11 @@ const IndexPage = ({ data }) => {
     }
   ));
 
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-
   return (
     <Layout>
       <SEO title="Overview" />
       <Header as="h1">Overview</Header>
-      <Gallery photos={photos} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={photos.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
+      <LightboxGallery photos={photos} />
     </Layout>
   )
 }
