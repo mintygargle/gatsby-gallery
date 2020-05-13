534552d60f56b60238aecc3e60426448e72ef2f7
Michael <michaelmontytanner@gmail.com>
2020-05-13 15:24:15 -0500

    API url

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: 'https://warm-fortress-36027.herokuapp.com',
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'image',
           'tag'
